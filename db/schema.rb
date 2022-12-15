1c1c811b780e4159e5d1b1922f99b09cabf7883d
Josh Hawks <snl95@txstate.edu>
2022-12-15 14:02:22 -0600

    rename role column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_15_195429) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_195917) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -72,7 +72,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_15_195429) do
   end
 
   create_table "roles", force: :cascade do |t|
-    t.integer "role", default: 0
+    t.integer "level", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
