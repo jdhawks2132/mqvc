817bc420389d40d91e0aa5c2453c28a4d604be50
Joshua Hawks <jdhawks@gmail.com>
2023-02-04 14:17:04 -0600

    adding mailer models

---
+++ db/migrate/20230204201608_create_mailers.rb
@@ -0,0 +1,10 @@
+class CreateMailers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :mailers do |t|
+      t.string :subject
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
